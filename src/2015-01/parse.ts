9cc362c6517bbaa4e7a7d490955742a1da8bad69
= <=>
2023-08-27 13:15:58 -0500

    Implemented parse function

--- src/2015-01/parse.ts
+++ src/2015-01/parse.ts
@@ -1,3 +1,9 @@
-export const parse = (input: string): number => {
-    return 0
-}
+export const parse = (input: string): number => input.split('').reduce((acc, char) => {
+    if (char === '(') {
+        return acc + 1
+    } else if (char === ')') {
+        return acc - 1
+    } else {
+        throw new Error(`Unexpected character: ${char}`)
+    }
+}, 0)
